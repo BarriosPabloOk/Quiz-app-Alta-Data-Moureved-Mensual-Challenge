3fc068247fe3991c21fd43be33766139084c472c
Pablo Barrios <pablob1518@hotmail.com>
2022-03-21 21:38:13 -0300

    commit with hilt's errors resolved

--- app/src/main/java/com/pablobarriosdevs/altadata/feature_quiz/domain/use_cases/SaveNewPlayer_UC.kt
+++ app/src/main/java/com/pablobarriosdevs/altadata/feature_quiz/domain/use_cases/SaveNewPlayer_UC.kt
@@ -1,5 +1,6 @@
 package com.pablobarriosdevs.altadata.feature_quiz.domain.use_cases
 
+import com.pablobarriosdevs.altadata.feature_quiz.domain.model.Player
 import com.pablobarriosdevs.altadata.feature_quiz.domain.repository.QuizRepository
 import javax.inject.Inject
 
@@ -8,7 +9,7 @@ class SaveNewPlayer_UC @Inject constructor(
     private val repository: QuizRepository
 ) {
 
-    suspend operator fun invoke(name:String, score:Int){
-        repository.savePlayer(name, score)
+    suspend operator fun invoke(player: Player){
+        repository.savePlayer(player)
     }
 }
